408a449c22941a93d846d023f7b29bb8098fd5fe
Merdan TURAN <merdanturan@gmail.com>
2021-11-15 22:10:53 +0300

    Create component

---
+++ src/components/sale/sale.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+
+const Sale = () => {
+    return (
+        <div className="main-sale">
+            <p className="percent">
+                55%
+            </p>
+            <p className="summer-sale">
+                SUMMER SALES
+            </p>
+        </div>
+    )
+}
+
+export default Sale
